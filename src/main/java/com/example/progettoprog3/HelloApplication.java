5481836560a5da9ca3ae26d01f40f7958934743b
marvel asuenimhen - 1026737 <marvelagene38@gmail.com>
2023-11-14 17:39:45 +0100

    Aggiornamento della main view(aggiunta icona)

--- src/main/java/com/example/progettoprog3/HelloApplication.java
+++ src/main/java/com/example/progettoprog3/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1080, 584);
+        Scene scene = new Scene(fxmlLoader.load(), 850, 584);
         stage.setTitle("Hello!");
         stage.initStyle(StageStyle.UNDECORATED);
         stage.setScene(scene);
