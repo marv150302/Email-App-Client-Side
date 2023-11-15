304ad34a0e77fc80bf1edfb601a2b16347c26fe9
marvel asuenimhen - 1026737 <marvelagene38@gmail.com>
2023-11-15 12:51:10 +0100

    added comments

--- src/main/java/com/example/progettoprog3/Controller.java
+++ src/main/java/com/example/progettoprog3/Controller.java
@@ -8,42 +8,80 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import model.*;
 
+/*
+* This is the controller class used to handle our views elements
+* @Author Marvel Asuenimhen
+* */
 public class Controller {
 
-    /*
+    /*-------------------------------------
+    * newEmailView
     * When you want to send a new email,
     * we open a new smaller view
     * which will contain the textfields and textareas,
     * for the receiver email, the email object, and the email text
     * as well as the sending and closing button(in case you don't want to send an email anymore)
+    * @FXML
     * */
     @FXML
     private AnchorPane newEmailView;
 
-    /*
-    *
+    /*-------------------------------------
+    * mainView
     * this is the first view you see when you open
     * the application
+    * @FXML
     * */
     @FXML
     private AnchorPane mainView;
 
-    /*
+    /*-------------------------------------
+    * closeNewEmailButton
     * this is the  button used to close the
     * "new email view" mentioned above,
     * which will be used to close the view if we don't want to send the email
+    * @FXML
     * */
     @FXML
     private Button closeNewEmailButton;
+
+    /*-------------------------------------
+    * newEmailButton
+    * This button is used to create a new Email
+    * @FXML
+    * */
     @FXML
     private Button new_email_button;
+
+    /*-------------------------------------
+    * inboxButton
+    * this button is (for now the only one avalible on the side menu)
+    * that allows the user to view previous and new emails
+    *@FXML
+    * */
     @FXML
     private Button inbox_button;
+
+    /*-------------------------------------
+    * left_menu
+    * the left side menu which contains the inbox button
+    * mentioned above
+    * */
     @FXML
     private VBox left_menu;
+
+    /*-------------------------------------
+    * receiver_email
+    * this is a textField used to get the receiver's email
+    * typed by the sender
+    * */
     @FXML
     private TextField receiver_email;
 
+    /*
+    * Object of type model,
+    * used to handle the model class
+    * */
     private DataModel model = new DataModel();
 
     @FXML
@@ -62,23 +100,10 @@ public class Controller {
         newEmailView.setVisible(true);
     }
 
-    protected void setBlurredBackground(){
-
-        BoxBlur bb = new BoxBlur();
-        bb.setWidth(5);
-        bb.setHeight(5);
-        bb.setIterations(2);
-
-        /*
-         * adding a blur effect
-         */
-
-        inbox_button.setEffect(bb);
-        left_menu.setEffect(bb);
-        //mainView.setEffect(bb);
-        //inbox_button.setEffect(null);
-    }
-
+    /*
+    * InitModel
+    * Used to make binding with elements of our view
+    * */
     public void initModel(){
 
         receiver_email.textProperty().bindBidirectional(model.getReceiver_email());
